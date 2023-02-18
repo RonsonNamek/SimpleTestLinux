717c2d9ab4aafe32a5910a143f0d408b7ea490b4
nameksirius <orz.sirius@gmail.com>
2023-02-18 23:29:37 +0800

    调整

---
+++ include/version.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace simpletest::version {
+
+static constexpr int Major = 1;
+static constexpr int Minor = 2;
+static constexpr int Patch = 3;
+static constexpr int Git   = 20230214;
+
+} // namespace simpletest::version
---
+++ src/simpletest/main.cpp
@@ -0,0 +1,58 @@
+#include <cstdlib>
+#include <iostream>
+#include <set>
+
+#include "glog/logging.h"
+#include "simpletest/nameofenum.h"
+#include "version.h"
+
+namespace {
+enum class Color : uint16_t {
+    White,
+    Red,
+    Green,
+    Blue,
+};
+}
+
+#define LOG_TO_STDERR_ONLY true
+
+int main(int argc, char* argv[])
+{
+    // Initialize Google's logging library.
+#if LOG_TO_STDERR_ONLY
+    FLAGS_colorlogtostderr = true;
+    FLAGS_logtostderr = true;
+#else
+    FLAGS_alsologtostderr = true;
+    FLAGS_log_dir = "./";
+#endif
+
+    google::EnableLogCleaner(7);
+    google::InitGoogleLogging(argv[0]);
+
+    LOG(INFO) << argv[0] << " version:" << simpletest::version::Major << "."
+              << simpletest::version::Minor << "." << simpletest::version::Patch
+              << "." << simpletest::version::Git;
+
+    std::set<uint32_t, std::greater<uint32_t>> testSet { 1, 3, 5, 7 };
+    auto it1 = testSet.lower_bound(0);
+    auto it2 = testSet.lower_bound(2);
+    auto it3 = testSet.lower_bound(3);
+    auto it4 = testSet.lower_bound(8);
+    std::cout << "lower 0:" << *it1 << ", lower 2:" << *it2
+              << ", lower 3:" << *it3 << ", lower 8:" << *it4 << std::endl;
+    std::cout << "lower 0 end?:" << (it1 == testSet.cend())
+              << ", lower 8 end?:" << (it4 == testSet.cend())
+              << ", *end:" << *(testSet.cend()) << std::endl;
+
+    constexpr uint32_t oneweek = 7 * 24 * 3600;
+    // constexpr size_t szConst = std::strlen("test");
+    constexpr size_t szConst2 = std::char_traits<char>::length("test2");
+    constexpr std::string_view testName = ReflectionEnum<Color, Color::Blue>().valueName;
+    std::cout << "1:" << oneweek << ", 2:" << szConst2 << ", 3:" << testName
+              << std::endl;
+
+    google::ShutdownGoogleLogging();
+    return EXIT_SUCCESS;
+}
